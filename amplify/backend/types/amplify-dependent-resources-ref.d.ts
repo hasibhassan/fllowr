63114ab3226553e081b49d4b55481a63a7c3b27b
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-17 19:08:42 -0600

    refactor: configure aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fllowr66002ebc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
